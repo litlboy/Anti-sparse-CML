8a7199f3643d7ca74d613c8b83a6cd5df75c6742
Adrien Vacher <litlboy@air-de-adrien-2.home>
2020-04-11 19:27:15 +0200

    MORE HYPERPARAMS

--- scripts/ch/evaluate.py
+++ scripts/ch/evaluate.py
@@ -3,7 +3,7 @@ from models.Pointwise.ch import CH
 import tensorflow as tf
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.15)
 max_users_eval = 7000
-datasets = ['ml-1m']
-r = ['r=8']
+datasets = ['ml-1m', 'amazon-CDs']
+r = ['r=8', 'r=16', 'r=32', 'r=64', 'r=128', 'r=256']
 
 eval_routine(CH, 'ch', datasets, r, max_users_eval, gpu_options)
